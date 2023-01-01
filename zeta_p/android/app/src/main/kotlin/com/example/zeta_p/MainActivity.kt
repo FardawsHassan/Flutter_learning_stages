54c1175102a51206a452fa5a4f0087d0f8de3c68
FardawsHassan <fardawshassanshuvro@gmail.com>
2023-01-01 15:39:48 +0600

    Step 4: A Quiz app

---
+++ zeta_p/android/app/src/main/kotlin/com/example/zeta_p/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zeta_p
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ zeta_p/lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quizzer',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Icon> scores = [];
+  void check_ans(bool ans) {
+    if (ans) {
+      scores.add(
+        Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      );
+    } else {
+      scores.add(
+        Icon(
+          Icons.close,
+          color: Colors.red,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Center(
+                    child: Text(
+                      'You can lead a cow down stairs but not up stairs.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(8, 0, 8, 18),
+                color: Colors.green,
+                height: 60,
+                child: TextButton(
+                  style: ButtonStyle(),
+                  onPressed: () {
+                    setState(() {
+                      check_ans(true);
+                    });
+                  },
+                  child: Text(
+                    "True",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(8, 0, 8, 18),
+                color: Colors.red,
+                height: 60,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      check_ans(false);
+                    });
+                  },
+                  child: Text(
+                    "False",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: scores,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

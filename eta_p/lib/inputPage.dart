13982e64841eb74b56ecc1aead89b92ce870f210
FardawsHassan <fardawshassanshuvro@gmail.com>
2023-01-05 16:33:34 +0600

    Input page screen UI updated.

--- eta_p/lib/inputPage.dart
+++ eta_p/lib/inputPage.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+enum gender {
+  male,
+  female,
+}
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -22,8 +28,13 @@ class _inputPageState extends State<InputPage> {
           Expanded(
             child: Row(
               children: [
-                inputCard(),
-                inputCard(),
+                inputCard(
+                  cardContent: inputCardContent(FontAwesomeIcons.mars, 'MALE'),
+                ),
+                inputCard(
+                  cardContent:
+                      inputCardContent(FontAwesomeIcons.venus, 'FEMALE'),
+                ),
               ],
             ),
           ),
@@ -43,7 +54,7 @@ class _inputPageState extends State<InputPage> {
             ),
           ),
           Container(
-            margin: EdgeInsets.only(top: 8),
+            margin: EdgeInsets.only(top: 7.5),
             width: double.infinity,
             height: 60.0,
             color: Color(0xffEA1556),
@@ -62,17 +73,50 @@ class _inputPageState extends State<InputPage> {
   }
 }
 
+class inputCardContent extends StatelessWidget {
+  IconData icon;
+  String iconLebel;
+  inputCardContent(this.icon, this.iconLebel) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          iconLebel,
+          style: TextStyle(
+            fontSize: 18,
+            color: Color(0xff8d8d98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class inputCard extends StatelessWidget {
-  const inputCard({
-    super.key,
-  });
+  Color background;
+  Widget? cardContent;
+  inputCard({this.cardContent, this.background = const Color(0xff1D1F33)}) {}
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
         margin: EdgeInsets.all(15),
-        color: Colors.grey,
+        decoration: BoxDecoration(
+          color: background,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: cardContent,
       ),
     );
   }
